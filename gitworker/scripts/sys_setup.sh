fa4f97c6800e1e09b54fd0174a825ebfabb37b3b
Jeczybula <kakoori@gmail.com>
2025-04-03 09:26:09 +0200

    refactor: Modify hash_reg to account for beacon/track calls
    Rename original hash_reg to track_hash_reg
    Add beacon_hash_reg

--- gitworker/scripts/sys_setup.sh
+++ gitworker/scripts/sys_setup.sh
@@ -7,9 +7,16 @@ echo "Preparing for cloning of $REMOTE_REPO_SSH_URL to $REMOTE_REPOSITORY_NAME"
 git clone $REMOTE_REPO_SSH_URL
 cd $REMOTE_REPOSITORY_NAME
 
-if [ ! -f ./hash_reg ]; then
-	cp ../hash_reg ./hash_reg
+if [ ! -f ./track_hash_reg ]; then
+	cp ../hash_reg ./track_hash_reg
 fi
+
+if [ ! -f ./beacon_hash_reg ]; then
+	cp ../hash_reg ./beacon_hash_reg
+fi
+
+
+
 
 
 git remote add github-repo $REMOTE_REPO_SSH_URL
