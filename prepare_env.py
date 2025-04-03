603b46ef3475525f3b749950dbf142740a7dad01
Jeczybula <kakoori@gmail.com>
2025-04-03 09:16:17 +0200

    chore: Remove obsolete $USERNAME variable
    Remove now obsolete $USERNAME variable from:
    - docker-compose.yaml
    - gitworker/Dockerfile
    - prepare_env.py

--- prepare_env.py
+++ prepare_env.py
@@ -3,12 +3,10 @@ import yaml
 with open('./secret.yaml', mode='r') as file:
     secret_yaml=yaml.load(file, yaml.Loader)
 
-USERNAME='gitworker'
 REMOTE_REPO_SSH_URL=secret_yaml['remote_repo']['remote_repository_url']
 REMOTE_REPO_NAME=REMOTE_REPO_SSH_URL.rstrip('.git').split('/')[-1]
 
 env_list=[
-        f"USERNAME='{USERNAME}'",
         f"REMOTE_REPO_SSH_URL='{REMOTE_REPO_SSH_URL}'",
         f"REMOTE_REPO_NAME='{REMOTE_REPO_NAME}'"
         ]
