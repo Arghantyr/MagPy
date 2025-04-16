9f72fda6e0efcdc321721db76204f80ea3c6ec7d
Jeczybula <kakoori@gmail.com>
2025-04-16 22:21:41 +0200

    feat: Add mapping binder
    Add id:value mapping binder in preparation for refactor using dict.update(map) instead of dict[id]=value

--- gitworker/scripts/Utils.py
+++ gitworker/scripts/Utils.py
@@ -1,10 +1,19 @@
 from hashlib import sha1
+import json
 
+
+NULL_IDENTIFIER="00000000-0000-0000-0000-000000000000"
 
 
 def get_hash(content:str)->str:
     try:
         return sha1(content.encode('utf-8')).hexdigest()
+    except Exception as e:
+        raise Exception(f"{e}")
+
+def bind_values_to_map(identifier:str=NULL_IDENTIFIER, value:dict={})->dict:
+    try:
+        return { identifier: get_hash(json.dumps(value, ensure_ascii=False)) }
     except Exception as e:
         raise Exception(f"{e}")
 
